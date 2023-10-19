4acdad0aeb6f1329aefeab30b45a747b3c4534c5
anasofiabeltran <anas.beltran206@gmail.com>
2023-10-19 11:25:48 -0500

    implements leaflet and the ip's API

--- IPAddressTracker/AngularV/src/app/Components/footer/footer.component.ts
+++ IPAddressTracker/AngularV/src/app/Components/footer/footer.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { APIService } from 'src/app/services/api.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, AfterViewInit {
+  private map: any;
 
+  constructor(public apiService: APIService) {}
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+  }
+
+  ngOnInit(): void {
+    this.apiService.getInfo();
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
--- IPAddressTracker/AngularV/src/app/services/api.service.ts
+++ IPAddressTracker/AngularV/src/app/services/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getInfo(){
+    this.http.get('https://geo.ipify.org/api/v2/country?apiKey=at_SPZjvAljqcEknV5yrG7R2d1uVm1E6&ipAddress=186.0.1.100').subscribe(data=>{
+      console.log(data);
+    })
+  }
 }
